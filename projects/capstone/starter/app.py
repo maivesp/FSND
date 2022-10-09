0091b93b98ecf55962e1d37fe78bee312e69a076
Shanmuga Prabhu <shanmugaprabhu.maivel@natwest.com>
2022-10-09 20:24:27 +0530

    successfully configured auth0 roles and invoked API from postman

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -1,11 +1,171 @@
 import os
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, jsonify, abort
+from sqlalchemy import exc
+import json
+import sys
 
-from models import *
+
+from models import setup_db,db_drop_and_create_all, Movie, Actor, Movie_cast
+from flask_cors import CORS
+from auth import AuthError, requires_auth
+
+app = Flask(__name__)
+CORS(app)
+setup_db(app)
+
+
+
+# ROUTES
 
 @app.route('/')
-def fun():
-    return("Hello World")
+def hello_world():
+    return 'Hello World'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+@app.route('/actor',methods=['GET'])
+@requires_auth(permission='get:actor')
+def get_actor(payload):
+    try:
+        actors = Actor.query.all()
+  #      print(actors, file=sys.stderr,flush=True)
+    except Exception as error:
+        print(str(error))
+        abort(404)
+
+    formatted_actors = [actor.format() for actor in actors]
+    return jsonify({
+        "success" : True,
+        "actors" : formatted_actors
+        })
+
+@app.route('/movie',methods=['GET'])
+@requires_auth(permission='get:movie')
+def get_movie(payload):
+    try:
+        movies = Movie.query.all()
+  #      print(actors, file=sys.stderr,flush=True)
+    except Exception as error:
+        print(str(error))
+        abort(404)
+
+    formatted_movies = [movie.format() for movie in movies]
+    return jsonify({
+        "success" : True,
+        "movies" : formatted_movies
+        })
+
+
+@app.route('/actor',methods=['POST'])
+@requires_auth(permission='create:actor')
+
+def create_actor(payload):
+    body=request.get_json()
+    
+    new_name=body.get("name")
+    new_age=body.get("age")
+    new_gender=body.get("gender")
+    actor=Actor(title=new_name,age=new_age,gender=new_gender)
+    
+    try:
+        actor.insert()
+    except Exception as error:
+        print(str(error.orig) + " for parameters" + str(error.params))
+        abort(422)
+    
+    return jsonify({
+        "success": True
+    })
+
+@app.route('/actor/<int:id>',methods=['PATCH'])
+@requires_auth(permission='update:actor')
+
+def update_actor(payload,id):
+
+    try:
+        actor=Actor.query.filter(Actor.id==id).one_or_none()
+
+    except Exception as error:
+        print(str(error))
+        abort(422)
+
+    if actor is None:
+        abort(404)
+    else:
+        body=request.get_json()
+
+        actor.name = body.get("name")
+        actor.age = json.dumps(body.get("recipe"))
+
+        try:
+            actor.update()
+
+        except Exception as error:
+            print(str(error.orig) + "for parameters" + str(error.params))
+            abort(422)
+
+    return jsonify({
+        "success" : True,
+        "actor" : [drink.long()]
+        })
+
+
+@app.route('/actor/<int:id>',methods=['DELETE'])
+@requires_auth(permission='delete:actor')
+
+def delete_actor(payload,id):
+    try:
+        actor=Actor.query.filter(Actor.id==id).one_or_none()
+
+    except Exception as error:
+        print(str(error))
+        abort(422)
+
+    if actor is None:
+        abort(404)
+    else:
+        try:
+            actor.delete()
+        except Exception as error:
+            print(str(error))
+            abort(422)
+
+    return jsonify({
+        "success":True,
+        "delete":id
+        })
+
+
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 422,
+        "message": "unprocessable"
+    }), 422
+
+@app.errorhandler(404)
+def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "resource not found"
+    }), 422
+
+@app.errorhandler(401)
+def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 401,
+        "message": "Not Authorized"
+    }), 401
+
+@app.errorhandler(AuthError)
+def handle_auth_error(ex):
+    """
+    Receive the raised authorization error and propagates it as response
+    """
+    response = jsonify(ex.error)
+    response.status_code = ex.status_code
+    return response
+
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=8080, debug=True)
--- projects/capstone/starter/models.py
+++ projects/capstone/starter/models.py
@@ -1,22 +1,32 @@
 
-
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_cors import CORS
+
+db= SQLAlchemy()
+# Test db setup
+database_filename = "database.db"
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
+# End of test db setup
+
+def setup_db(app):
+
+  #app.config.from_object('config')
+  app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+  app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+  db.app=app
+  db.init_app(app) 
+
+  migrate = Migrate(app, db)
 
 
-def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
 
-  return app
 
-app = create_app()
-app.config.from_object('config')
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 #----------------------------------------------------------------------------#
 # Models.
@@ -28,27 +38,98 @@ class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     release_date = db.Column(db.Date)
+    actors_in_movie = db.relationship('Movie_cast',backref='actors_in_movie',lazy=True)
 
     def __repr__(self):
         return f'''<Movie { self.id} { self.title} { self.release_date} >'''
 
+    def __init__(self, tile, release_date):
+        self.title = tile
+        self.release_date = release_date
+ 
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title
+            }
 
 
 class Actor(db.Model):
     __tablename__ = 'actor'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
     age = db.Column(db.Integer)
     gender = db.Column(db.String(6))
+    movies_acted_by = db.relationship('Movie_cast',backref='movies_acted_by',lazy=True)
+
     def __repr__(self):
         return f'''<Actor { self.id } { self.name } { self.age } { self.gender }>'''
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+ 
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+            }
 
 class Movie_cast(db.Model):
     __tablename__ = 'movie_cast'
     id = db.Column(db.Integer,primary_key=True)
-    movie_id = db.Column(db.String)
-    actor_id = db.Column(db.Integer)
-    movie_actor = db.relationship('Actor',backref='actors',lazy=True)
-    actor_movie = db.relationship('Movie',backref='movies',lazy=True)
-   
+    movie_id = db.Column(db.Integer,db.ForeignKey('movie.id'))
+    actor_id = db.Column(db.Integer,db.ForeignKey('actor.id'))
 
+    def __repr__(self):
+        return f'''<Actor { self.id } { self.movie_id } { self.actor_id } >'''
+
+    def __init__(self, movie_id, actor_id):
+        self.movie_id = movie_id
+        self.actor_id = actor_id
+ 
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'movie_id': self.movie_id,
+            'actor_id': self.actor_id,
+            }
